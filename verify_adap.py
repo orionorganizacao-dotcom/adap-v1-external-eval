27cdc80b94dd14e2a4e02c52578a063f157fc145
orionorganizacao-dotcom <orionorganizacao@gmail.com>
2026-03-29 12:18:05 -0300

    Update verify_adap.py

--- verify_adap.py
+++ verify_adap.py
@@ -1,6 +1,6 @@
 import hashlib
 
-EXPECTED_HASH = "PENDENTE"
+EXPECTED_HASH = "4cf7f0460b8334e0b9df94c5610ef90ebc163985900b4d1ecb29799367538919"
 
 with open("adap_ledger.json", "rb") as f:
     computed_hash = hashlib.sha256(f.read()).hexdigest()
